089733a6d39ba19d49484605bcb95fb9f5ec7a6a
Justin Goulet <jstngoulet@me.com>
2016-11-21 14:27:43 -0800

    backup from latptop

--- Old Version/js/loadBeerList.js
+++
@@ -1,6 +0,0 @@
-function loadBeerList() {
-    var bodyArray = document.getElementsByClassName("beers");
-    for (var i = 0; i < bodyArray.length; i++) {
-        bodyArray[i].innerHTML = '<object type="text/html" width="95%" height="400" data="BeerInfo.html" ></object>';
-    }
-}
