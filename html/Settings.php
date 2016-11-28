0704f7d717f6eefd9b3f21d28f801845452c9124
mikal <mikal.callahan@icloud.com>
2016-11-27 21:06:46 -0800

    settings php

--- html/Settings.php
+++ html/Settings.php
@@ -22,88 +22,97 @@
 		</script>
 
 		<?php
-			session_start(); // start connection
+            session_start(); // start connection
 
-			/*Get the token to prove the user was logged in*/
-			  if(strlen($_SESSION['loginToken']) == 0){ // if the user is not logged in
-			 header("Location: ../index.php"); // redirect to index.php
-		}
-			 // Create a basic connection
-		    $connection = mysqli_connect("localhost", "calla033", "CAss1007", "team3");
+            /*Get the token to prove the user was logged in*/
+              if (strlen($_SESSION['loginToken']) == 0) { // if the user is not logged in
+             header('Location: ../index.php'); // redirect to index.php
+              }
+             // Create a basic connection
+            $connection = mysqli_connect('localhost', 'calla033', 'CAss1007', 'team3');
 
-		    // Check the connection
-		    if(mysqli_connect_errno()){
-		        die("Connection Failed. ERR: " . mysqli_connect_error());
-		    }
-			function submitChange(){
-				echo "testest";
-				$sql = "UPDATE Users SET FName='fname',  LName='lname', Email='email, Password='password WHERE Email=$Email";
-				if(mysqli_query($connection,$sql)){
-					echo " records updated";
-				}
-				mysql_error($connection);
-			/*	UPDATE Users SET FName='fname', LName='lname', Email='email', Password='password' WHERE Email=$Email;*/
-		}
-		?>
+            // Check the connection
+            if (mysqli_connect_errno()) {
+                die('Connection Failed. ERR: '.mysqli_connect_error());
+            }
+
+            $GetUserInformationQuery = "SELECT * FROM Users WHERE Email='".$_SESSION['currentUser']."'";
+            $userInfoResults = mysqli_query($connection, $GetUserInformationQuery);
+
+            if ($userInfoResults->num_rows > 0) {
+                while ($row = mysqli_fetch_assoc($userInfoResults)) {
+                    $FName = $row['FName'];
+                    $LName = $row['LName'];
+                    $Email = $row['Email'];
+                    $Password = $row['Password'];
+                    break;
+                }
+            }
+        ?>
 
 </head>
 
 <body>
 	<div class="accountsettings">
-		<h2 id="accountsettingstitle"> Account Settings </h2>
-		<!-- account settings header -->
-		<h3> General Settings: </h3>
-		<!-- header for gs -->
-		<div class="generalsettings"><br>
-			<!-- general settings class -->
-			<form onsubmit="return false;">
+		<h2 id="accountsettingstitle"> Account Settings </h2> <!-- account settings header -->
+		<h3> General Settings: </h3> <!-- header for gs -->
+		<div class="generalsettings"><br> <!-- general settings class -->
 				<?php
-				$GetUserInformationQuery = "SELECT * FROM Users WHERE Email='" . $_SESSION["currentUser"] . "'";
-				$userInfoResults = mysqli_query($connection, $GetUserInformationQuery);
-
-				//Check to see if exists (it should since we already logged in)
-				if($userInfoResults-> num_rows > 0){
-					while($row = mysqli_fetch_assoc($userInfoResults)){
-						$FName = $row["FName"];
-						$LName = $row["LName"];
-						$Email = $row["Email"];
-						break; //Only want the first occurance
-					}
-				}else{
-					//Error getting info
-				}
-				?>
-				<!-- general settings form -->
-				<div>First Name: </div>
-				<!-- first name label -->
-				<input class="leftAlign" id="fname" type="text" name="fname" value="<?php echo $FName; ?>"><br>
-				<!-- first name form -->
-				<div>Last Name: </div>
-				<!-- last name label -->
-				<input class="leftAlign" id="lname" type="text" name="lname" value="<?php echo $LName; ?>"><br>
-				<!-- first name form -->
-				<div>Email: </div>
-				<!-- email label -->
-				<input class="leftAlign" id="email" type="email" name="email" value="<?php echo $Email; ?>"><br>
-				<!--email form -->
-				<div>Password: </div>
-				<!-- password label -->
-				<input class="leftAlign" id="password" type="password" name="password" placeholder="*********"><br>
-				<!-- password form -->
-				<!-- end gernal settings form -->
-				<button onclick="<?php submitChange()?>" class="inputbutton">Confirm</button>
-			</form>
-
-			<!-- onclick do something -->
+                    /* If the user has info */
+                    if ($userInfoResults->num_rows > 0) {
+                        /* echo the form data for them */
+                        echo "<form action='' class='stdForm' method='POST'' name='generalsettings'>";
+                        echo '<div>First Name: </div>';
+                        echo "<input class='leftAlign' id='fname' type='text' name='fname' value='$FName'><br>";
+                        echo '<div> Last Name: </div>';
+                        echo "<input class='leftAlign' id='lname' type='text' name='lname' value='$LName'><br>";
+                        echo '<div> Email: </div>';
+                        echo "<input class='leftAlign' id='email' type='text' name='email' value='$Email'><br>";
+                        echo '<div> Password: </div>';
+                        echo "<input class='leftAlign' id='password' type='password' name='password' value='$Password'><br>";
+                        echo '<button type="submit" name="generalsettings">Comfirm</button>';
+                        echo '</form>';
+                    } else {
+                        //Error getting info
+                    }
+                if ($_SERVER['REQUEST_METHOD'] == 'POST') { // if a post request was found
+                    if (isset($_POST['generalsettings'])) { // and it was from general settings
+                        $fn = $_POST['fname']; // fn = new first name
+                        $ln = $_POST['lname']; // ln = new last name
+                        $em = $_POST['email']; // em = new email
+                        $pw = $_POST['password']; // pw = new password
+                        $updateUserInfo = "UPDATE Users Set FName = '".$fn."', LName = '".$ln."', Email = '".$em."', Password = '".$pw."' WHERE Email = '".$_SESSION['currentUser']."'"; // get update statement
+                        if (mysqli_query($connection, $updateUserInfo)) {
+                            echo 'Records updated<br>';
+                            echo "<br>$updateUserInfo";
+                        } else {
+                            echo '<br><br><br>';
+                            echo "$updateUserInfo";
+                            echo 'ERORRRR';
+                        }
+                    }
+                }
+                ?>
 		</div>
 		<!-- end general settings -->
 
-		<h3> My Breweries: </h3>
-		<!-- header for my breweries -->
-		<div class="mybreweries"><br>
-			<!-- my breweries class -->
-			You have don't have any breweries
-			<br><br><button type="submit" onclick="location.href='BrewerySignUp.php'">Add Brewery</button>
+		<h3> My Breweries: </h3>n<!-- header for my breweries -->
+		<div class="mybreweries"><br> <!-- my breweries class -->
+			<?php
+                $GetUserInformationQuery = "SELECT * FROM BreweryOwner WHERE UserEmail='".$_SESSION['currentUser']."'"; // get user info query
+                $userInfoResults = mysqli_query($connection, $GetUserInformationQuery); // get user info
+                if ($userInfoResults->num_rows > 0) { // if there are results
+                    while ($row = mysqli_fetch_assoc($userInfoResults)) {
+                        $OwnerID = $row['OwnerID']; // OwnerID = OwnerID
+                        $UserEmail = $row['UserEmail']; // UserEmail = UserEmail
+                        $BreweryID = $row['BreweryID']; // BreweryID = BreweryID
+                        break;
+                    }
+                } else { // the user has no breweries
+                    echo 'You do not have any breweries'; // Output the user does not have a brewery
+                }
+            ?>
+			<br><br><button type="submit" onclick="location.href='BrewerySignUp.php'">Add Brewery</button> <!-- link to create brewery -->
 		</div>
 		<!-- end my breweries -->
 
