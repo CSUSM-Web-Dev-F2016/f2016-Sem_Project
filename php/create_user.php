22a4ca64e5be21bd2617ad32b0abb0b6c5f0a0c8
chrislarsen <cjl0749@gmail.com>
2016-11-29 17:59:06 -0800

    Removing git stuff

--- php/create_user.php
+++ php/create_user.php
@@ -19,24 +19,18 @@ session_start();
 function createUser($FName, $LName, $birthday, $Email, $Password, $ProfilePicURl){
     echo "Creating User";
     //Create the user in an SQL Command, tehn, log them in.
-    $createUserQuery = "INSERT INTO Users (Email, Password, FName, LName, DOB, ProfilePicURL, LastLogin)
-        VALUES ('" . $Email . "', '" . $Password . "', '" . $FName . "', '" . $LName . "', '" . $birthday . "', '" . $ProfilePicURl . "', UTC_TIMESTAMP())";
+    $createUserQuery = "INSERT INTO Users (Email, Password, FName, LName, DOB, ProfilePicURL) 
+        VALUES ('" . $Email . "', '" . $Password . "', '" . $FName . "', '" . $LName . "', '" . $birthday . "', '" . $ProfilePicURl . "')";
 
     // get database connection
     $connection = include 'DBConnectionReturn.php';
 
     if(mysqli_query($connection, $createUserQuery)){
         //The item was successful created, now change the login page.
-<<<<<<< HEAD
         echo "<p style=\"text-align:center; color:green; width:100%; font-size:18px;\">Successfully inserted data: $Email</p>";
         $_SESSION['currentUser'] = $_SESSION['signedInUser'] = $Email;
 
         //Now, set the authorization token value so we know the user is logged in
-=======
-        $_SESSION['currentUser'] = $_SESSION['signedInUser'] = $Email;
-
-        //Now, set the autoriazation token value so we know the user is logged in
->>>>>>> master
         $_SESSION['loginToken'] = "yes";
 
         //Redirect to the main profile page
@@ -47,8 +41,4 @@ function createUser($FName, $LName, $birthday, $Email, $Password, $ProfilePicURl
         echo "<p style=\"text-align:center; color:red; width:100%; font-size:18px;\">Error with creating account: <br>" . $connection->error . "</p>";
     }
     $connection->close();
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> master
