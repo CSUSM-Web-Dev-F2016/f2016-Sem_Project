4bb703c0b9ef83f863baa3a88cd6dd5af452dbc4
Justin Goulet <jstngoulet@me.com>
2016-12-04 17:46:32 -0800

    fixed alignment of breweryPage.php onsubmit post function

--- html/breweryPage.php
+++ html/breweryPage.php
@@ -371,50 +371,50 @@
 		//Check which form was set then get the appropriate id.
 		if(isset($_POST['brewery'])){
 			//Navigate to the brewery page iwth the new id
+			CustomLog($connection, $_SESSION['signedInUser'], 'User Action', "User Visited BreweryID=" . end(array_keys($_POST)) . "");
 			echo "<script type=\"text/javascript\"> document.location.href = \"breweryPage.php?id=" . end(array_keys($_POST)) . "\";</script>";
 		}
 		else if(isset($_POST['followBrew'])){
 
-								CustomLog($connection, $_SESSION['signedInUser'], 'User Action', "User Visited BreweryID=" . end(array_keys($_POST)) . "");
+				CustomLog($connection, $_SESSION['signedInUser'], 'User Action', "User Visited BreweryID=" . end(array_keys($_POST)) . "");
 
-							  	//Navigate to the brewery page iwth the new id
-							  	echo "<script type=\"text/javascript\"> document.location.href = \"breweryPage.php?id=" . end(array_keys($_POST)) . "\";</script>";
-    						}
-    						else if(isset($_POST['followBrew'])){
-								//User is going to Follow the user$
-								if($following == 'y'){
-									//If the user is currently following the user, unfollow it and change the image
-									$DeleteQuery = "DELETE FROM UserFollowsBrewery WHERE UserEmail='" . $signedInUser . "' AND BreweryID=$id";
-									if(mysqli_query($connection, $DeleteQuery)){
-										//Success
-										$followText = "Follow";
-										$followingImage = "../img/Follow.png?raw=true";
-										CustomLog($connection, $_SESSION['signedInUser'], 'User Action', "User Followed BreweryID=" . end(array_keys($_POST)) . "");
-										echo "<script type=\"text/javascript\"> document.location.href = \"breweryPage.php?id=" . $id . "\";</script>";
-									}else{
-										die("Error: " . mysqli_error($connection));
-									}
-								}else{
-									//If the user is not following the user, follow it and change the image.
-									$addQuery = "INSERT INTO UserFollowsBrewery (UserEmail, BreweryID) VALUES ('" . $signedInUser ."', '" . $id ."')";
-									if(mysqli_query($connection, $addQuery)){
-
-										$followText = "UnFollow";
-										$followingImage = "../img/Unfollow_Follow_Color.png?raw=true";
-										CustomLog($connection, $_SESSION['signedInUser'], 'User Action', "User Un-Followed BreweryID=" . end(array_keys($_POST)) . "");
-										echo "<script type=\"text/javascript\"> document.location.href = \"breweryPage.php?id=" . $id . "\";</script>";
-									} else{
-										die("Error: " . mysqli_error($connection));
-									}
-								}
-							}
-    						else {
-							    $_SESSION['currentUser'] = strtr(end(array_keys($_POST)), array('#-#' => '.'));
-									CustomLog($connection, $_SESSION['signedInUser'], 'User Visited', "" . $_SESSION['currentUser'] . "");
-									echo "<script type=\"text/javascript\"> document.location.href = \"profilePage.php\";</script>";
-						    }
-
+				//Navigate to the brewery page iwth the new id
+				echo "<script type=\"text/javascript\"> document.location.href = \"breweryPage.php?id=" . end(array_keys($_POST)) . "\";</script>";
+    }
+    else if(isset($_POST['followBrew'])){
+				//User is going to Follow the user$
+				if($following == 'y'){
+						//If the user is currently following the user, unfollow it and change the image
+						$DeleteQuery = "DELETE FROM UserFollowsBrewery WHERE UserEmail='" . $signedInUser . "' AND BreweryID=$id";
+						if(mysqli_query($connection, $DeleteQuery)){
+								//Success
+								$followText = "Follow";
+								$followingImage = "../img/Follow.png?raw=true";
+								CustomLog($connection, $_SESSION['signedInUser'], 'User Action', "User Followed BreweryID=" . end(array_keys($_POST)) . "");
+								echo "<script type=\"text/javascript\"> document.location.href = \"breweryPage.php?id=" . $id . "\";</script>";
+						}else{
+								die("Error: " . mysqli_error($connection));
+						}
+		}else{
+				//If the user is not following the user, follow it and change the image.
+				$addQuery = "INSERT INTO UserFollowsBrewery (UserEmail, BreweryID) VALUES ('" . $signedInUser ."', '" . $id ."')";
+				if(mysqli_query($connection, $addQuery)){
+						$followText = "UnFollow";
+						$followingImage = "../img/Unfollow_Follow_Color.png?raw=true";
+						CustomLog($connection, $_SESSION['signedInUser'], 'User Action', "User Un-Followed BreweryID=" . end(array_keys($_POST)) . "");
+						echo "<script type=\"text/javascript\"> document.location.href = \"breweryPage.php?id=" . $id . "\";</script>";
+				} else{
+						die("Error: " . mysqli_error($connection));
+					}
+				}
+			}
+    else {
+				$_SESSION['currentUser'] = strtr(end(array_keys($_POST)), array('#-#' => '.'));
+				CustomLog($connection, $_SESSION['signedInUser'], 'User Visited', "" . $_SESSION['currentUser'] . "");
+				echo "<script type=\"text/javascript\"> document.location.href = \"profilePage.php\";</script>";
 		}
+
+}
 
 		//Ends the current session
 		session_write_close();
